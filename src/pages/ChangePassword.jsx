9200683ce136056e11fafe1faa08a576b8f13960
umeadipaul53 <umeadipaul53@gmail.com>
2025-06-19 02:57:25 +0100

    corrected vercel failure

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import API from "../api/api";
 import Swal from "sweetalert2";
 import React, { useEffect, useState } from "react";
